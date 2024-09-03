06da251f1682443a8a08e5578846173745495a62
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-09-03 02:49:01 -0700

    add weather App using openWeather API

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import CountDown from "./components/counter";
+import Weather from "./components/weather-widget";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CountDown/>
+      {/* <CountDown/> */}
+      <Weather/>
           </main>
   );
 }
