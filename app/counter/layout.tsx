98a93f67798b34feeb8bde0e11070efbe1c600ee
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-09-05 05:59:35 -0700

    remove counter file

--- app/counter/layout.tsx
+++ app/counter/layout.tsx
@@ -1,14 +1,14 @@
 
-export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <>
-      <h1 className="text-2xl text-center bg-gray-400">Count Timer</h1>
-      {children}
-      </>
-    );
-  }
+// export default function RootLayout({
+//     children,
+//   }: Readonly<{
+//     children: React.ReactNode;
+//   }>) {
+//     return (
+//       <>
+//       <h1 className="text-2xl text-center bg-gray-400">Count Timer</h1>
+//       {children}
+//       </>
+//     );
+//   }
   
